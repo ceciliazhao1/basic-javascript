eff3d1acab32358303ca55664d8596b43a0a57de
cecilia1 <belindazhao0130@gmail.com>
2023-09-07 23:29:57 -0700

    1

---
+++ index.js
@@ -0,0 +1,19 @@
+const object = new object()
+
+const person1={name: "ceco", hobby:"eat"}
+const person2={name: "ceco", hobby:"eat"}
+const person3={name: "ceco", hobby:"eat"}
+
+console.log(person1,person2,person3)
+console.table[(person1,person2,person3)]
+//console.log(%c)
+
+const deleteMe = () =>console.trace('delete it once!')
+
+deleteMe()
+deleteMe()
+
+function feed(human){
+    const {name, age, hobby} = human
+        return `FEED${name} of ${age} and ${hobby}
+}
