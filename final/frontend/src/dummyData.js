f78877b594dc709d55d412b91470c7c2c879ef13
cecilia1 <belindazhao0130@gmail.com>
2023-12-14 20:55:52 -0800

    final proj

---
+++ final/frontend/src/dummyData.js
@@ -0,0 +1,94 @@
+export const Users = [
+    {
+      id:1,
+      profilePicture: "person/1.jpg",
+      username: "Safak Kocaoglu",
+    },
+    {
+      id:2,
+      profilePicture: "person/1.jpg",
+      username: "Janell Shrum",
+    },
+    {
+      id:3,
+      profilePicture: "person/1.jpg",
+      username: "Alex Durden",
+    },
+    {
+      id:4,
+      profilePicture: "person/1.jpg",
+      username: "Dora Hawks",
+    },
+    {
+      id:5,
+      profilePicture: "person/1.jpg",
+      username: "Thomas Holden",
+    },
+    {
+      id:6,
+      profilePicture: "person/1.jpg",
+      username: "Shirley Beauchamp",
+    },
+  ];
+  
+  export const Posts = [
+    {
+      id: 1,
+      desc: "Love For All, Hatred For None.",
+      photo: "post/1.jpeg",
+      date: "5 mins ago",
+      userId: 1,
+      like: 32,
+      comment: 9,
+    },
+    {
+      id: 2,
+      photo: "post/1.jpeg",
+      date: "15 mins ago",
+      userId: 2,
+      like: 2,
+      comment: 1,
+    },
+    {
+      id: 3,
+      desc: "Every moment is a fresh beginning.",
+      photo: "post/1.jpeg",
+      date: "1 hour ago",
+      userId: 3,
+      like: 61,
+      comment: 2,
+    },
+    {
+      id: 4,
+      photo: "post/1.jpeg",
+      date: "4 hours ago",
+      userId: 4,
+      like: 7,
+      comment: 3,
+    },
+    {
+      id: 5,
+      photo: "post/1.jpeg",
+      date: "5 hours ago",
+      userId: 5,
+      like: 23,
+      comment: 5,
+    },
+    {
+      id: 6,
+      photo: "post/1.jpeg",
+      date: "1 day ago",
+      userId: 6,
+      like: 44,
+      comment: 6,
+    },
+    {
+      id: 7,
+      desc: "Never regret anything that made you smile.",
+      photo: "post/1.jpeg",
+      date: "2 days ago",
+      userId: 7,
+      like: 52,
+      comment: 3,
+    },
+  ];
